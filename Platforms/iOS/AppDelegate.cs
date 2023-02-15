52c1182ca6ebbec1117e513bb4a60912c55b2e9c
Alena Jonjić <105975496+alenajo@users.noreply.github.com>
2023-02-15 11:24:20 +0100

    add files

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RMA_Tim6;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
